392a8fe3a9f127a73e0d824160f80ee1c557a009
Aaron Kelleher <aaronkelleher@hotmail.co.uk>
2022-04-05 00:53:06 +0100

    Added code to identify differences in dataframes

--- data_processor.py
+++ data_processor.py
@@ -15,78 +15,64 @@ def read_new_data():
 
 def update_data():
 
-    master_df = get_master_data()
-    scraped_df = read_new_data()
-
-    ##########################################################
-    # To do 
-    # Remove what isnt in the new df from the old df
-    # Append new houses from new df to old df
-    # Update existing entries for price increases/decreases
-    ##########################################################
-
-    i=0
-    
-    # while i< len(scraped_df):
-    #     j=0
-    #     while j< len(master_df):
-    #         if ((master_df.iloc[j].daft_link)==(scraped_df.iloc[i].daft_link)):
-    #             print('hit')
-            
-    #         j=j+1
-    #     i=i+1
-        
-    ####################################
-
-    # master_df.set_index("daft_link")
-    # scraped_df.set_index("daft_link")
-
-    all_entries_df = pd.merge(master_df, scraped_df, on="daft_link", how="outer", indicator="True")
-
-    #in_master_not_in_new_scraped_df = all_entries_df.loc[lambda x: x['_merge'] == 'left_only']
-
-    in_master_not_in_new_scraped_df = master_df.merge(scraped_df, left_on="daft_link", right_on="daft_link")
-    other = scraped_df.merge(master_df, left_on="daft_link", right_on="daft_link")
-
-    # not_in_master_in_scraped_df = all_entries_df.loc[lambda x: x['_merge'] == 'right_only']
+    master_df = pd.read_csv('master_data.csv', header=0) # retain the headers
+    scraped_df = pd.read_csv('daft properties.csv', header=0)
 
     
-    print(other)
+
+    
+
+    # houses still for sale
+    in_both = scraped_df.merge(master_df, on="daft_link", indicator=True, how='left').loc[
+        lambda x: x['_merge'] == 'both']
+    # print(in_both)
+    # with open("still_for_sale.txt", "w") as output:
+    #     print(in_both.to_string(), file=output)
+
+    
+   # print(other)
 
     # Remove sold properties
-    master_df = remove_entries(master_df, scraped_df)
-    print('done')
+    # print('removing')
+    # master_df = remove_entries(master_df, scraped_df)
+    # print(master_df)
+    # print('removed')
 
     # Add new houses
-    scraped_df = add_new_entries(master_df, scraped_df)
-
+    print('adding')
+    print(master_df)
+    master_df = add_new_entries(master_df, scraped_df)
+    print('added')
+    print(master_df)
 
 def remove_entries(master_df, scraped_df):
 
-    i=0
-    while (i<len(scraped_df.index)):
+     # sold houses
+    only_in_master_not_in_scraped = master_df.merge(scraped_df, on="daft_link", indicator=True, how='left').loc[
+        lambda x: x['_merge'] != 'both']
+    # print(only_in_master_not_in_scraped)
+    # with open("sold.txt", "w") as output:
+    #     print(only_in_master_not_in_scraped.to_string(), file=output)
+    print(only_in_master_not_in_scraped)
 
-        ###############################
-        # identify duplicate entries ##
-        ###############################
+    condition = master_df['daft_link'].isin(only_in_master_not_in_scraped['daft_link'])
+    master_df=master_df.drop(master_df[condition].index, inplace = True)
 
-        #print(master_df.iloc[i].daft_link)
-        # if ((master_df.iloc[i].daft_link)==(scraped_df.iloc[i].daft_link)):
-        #     print('hit')
-
-        # link = master_df.daft_link[i]
-        # my_flag = scraped_df[scraped_df.eval('link == {}'.format(link))].flag
-        # print(my_flag)
-
-        #print(master_df.iloc[i])
-
-
-        i+=1
+    
     return master_df
 
 
-
 def add_new_entries(master_df, scraped_df):
+
+    # new houses
+    only_in_scraped_not_in_master = scraped_df.merge(master_df, on="daft_link", indicator=True, how='left').loc[
+        lambda x: x['_merge'] != 'both']
+    # print(only_in_scraped_not_in_master)
+    # with open("new.txt", "w") as output:
+    #     print(only_in_scraped_not_in_master.to_string(), file=output)
+    print('below is scraped only')
+    print(only_in_scraped_not_in_master)
+    master_df=master_df.append(only_in_scraped_not_in_master)
     
     return master_df
 
