d137592bf7ed4a21302d088378850ec2e77c6981
Aaron Kelleher <aaronkelleher@hotmail.co.uk>
2022-04-06 10:21:24 +0100

    Added debugging mode

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from cgi import test
 from flask import Flask, render_template, request, redirect
-import main as test_script
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    #test_script.hello()
-    #test_script.daft_scraper()
+
     return render_template("index.html")
 
 @app.route("/application", methods=["POST", "GET"])
 def login():
+
     if request.method == "POST":
         salary = request.form["salary"]
         age = request.form["age"]
         print(salary)
         print(age)
         return redirect("/results")
+
     else:
         return render_template("application.html")
 
