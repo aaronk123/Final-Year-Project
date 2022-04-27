06f6f0bd1a3bea93843aea00a9b8e83ac2c78318
Aaron Kelleher <aaronkelleher@hotmail.co.uk>
2022-04-27 19:55:22 +0100

    Added functionality for the results page

--- app.py
+++ app.py
@@ -2,47 +2,60 @@ from cgi import test
 from flask import Flask, render_template, request, redirect
 import scrapers.daft_scraper as daft_scraper
 import scrapers.pricePropertyRegister as ppr_scraper
+import matcher as matcher
 import time
 import threading
+import os
 
-def test_task():
-    while True:
-        print('daemon running')
-        #sleep for 1 day
-        # time.sleep(24*60)
-        # print('daemon finished sleeping')
-        # scrape_data()
+# def test_task():
+#     while True:
+#         print('daemon running')
+#         # sleep for 1 day
+#         time.sleep(24*60)
+#         print('daemon finished sleeping')
+#         # scrape_data()
         
-def scrape_data():
-    daft_scraper()
-    ppr_scraper()
-
+# def scrape_data():
+#     # daft_scraper()
+#     # ppr_scraper()
+#     print('scraping')
+    
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    thread = threading.Thread(target=test_task)
-    thread.daemon = True         # Daemonize 
-    thread.start()
+def index():
+    # thread = threading.Thread(target=test_task)
+    # thread.daemon = True         # Daemonize 
+    # thread.start()
     return render_template("index.html")
 
 @app.route("/application", methods=["POST", "GET"])
-def login():
+def application():
+    try:
+        os.remove("templates/property_map.html")
+    except Exception:
+        pass
+
     if request.method == "POST":
-        salary = request.form["salary"]
-        age = request.form["age"]
-        print(salary)
-        print(age)
+
+        budget = request.form["budget"]   
+        rooms= request.form["rooms"]
+        bathrooms=request.form["bathrooms"]
+        county=request.form["county_dropdown"]
+        stakeholder=request.form["stakeholder_group"]
+
+        matcher.match_data(budget, rooms, bathrooms, county, stakeholder)
+
         return redirect("/results")
+        # return render_template("property_map.html")
     else:
         return render_template("application.html")
 
 @app.route("/results", methods=["GET"])
 def results():
 
-    return "<p>results page</p>"
+    return render_template("property_map.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    app.run(debug=True)
 
-#https://python-adv-web-apps.readthedocs.io/en/latest/flask_forms.html#configure-the-form
